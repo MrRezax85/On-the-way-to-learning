1b7afc866916ad0320c2696ddf805e3c3f9b99d9
Mr.Reza <86867247+MrRezax85@users.noreply.github.com>
2023-01-03 09:55:25 +0330

    power of num

---
+++ power of numbers.py
@@ -0,0 +1,10 @@
+# The first number is the power of the second number
+# به توان رساندن عدد اولی به دومی
+print("Enter the numbers you want")
+
+A = float(input("First Number:"))
+B = float(input("Second Number:"))
+
+print("The first number is the power of the second number:")
+
+print(A**B)
