31089db426717a0b2052c0afde5bb97ebb96e79e
Mr.Reza <86867247+MrRezax85@users.noreply.github.com>
2023-01-03 09:55:58 +0330

    Average num

---
+++ Average.py
@@ -0,0 +1,16 @@
+#Average numbers
+#میانگین اعداد
+
+print("Enter the desired numbers to get its average")
+ 
+_num = int(input("Number of courses:"))
+
+input("v") * _num
+
+print(k)
+
+# A = float(input("The first number:"))
+# B = float(input("The second number:"))
+# C = float(input("The third number:"))
+
+# print( (A + B + C) / _num )
